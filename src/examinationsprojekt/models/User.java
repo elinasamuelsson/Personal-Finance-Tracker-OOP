2cd5062b716b4d264abe9e656282166b9de459a4
elinasamuelsson <elina.samuelsson@live.com>
2025-12-16 15:56:55 +0100

    Update User.java
    Adding getters and some helper methods to User model, and correcting constructor.

--- src/examinationsprojekt/models/User.java
+++ src/examinationsprojekt/models/User.java
@@ -1,5 +1,6 @@
 package examinationsprojekt.models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class User {
@@ -7,9 +8,29 @@ public class User {
     private String password;
     private List<Account> accounts;
 
-    public User(String username, String password, List<Account> accounts) {
+    public User(String username, String password) {
         this.username = username;
         this.password = password;
-        this.accounts = accounts;
+        this.accounts = new ArrayList<>();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void addAccountToList(Account account) {
+        accounts.add(account);
+    }
+
+    public void removeAccountFromList(Account account) {
+        accounts.remove(account);
     }
 }
